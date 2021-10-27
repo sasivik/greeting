8eba8a1230d6e52ec2b9c1d264d847d36516e09b
sasivik <sasikala.kuppuswamy@gmail.com>
2021-10-27 14:19:06 +0530

    first commit
    uploading all files

---
+++ buildrest/src/main/java/com/learn/demo/BuildrestApplication.java
@@ -0,0 +1,13 @@
+package com.learn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildrestApplication.class, args);
+	}
+
+}
